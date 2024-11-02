4728c07ba603e0057486374e5ac51d7b240b8cd4
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-02 16:03:29 +1100

    feat: move paddle following mouse

--- c_ncurses/blck.c
+++ c_ncurses/blck.c
@@ -9,13 +9,36 @@ int main(void) {
   // hide cursor
   curs_set(0);
 
+  // enable mouse event detection
+  keypad(stdscr, TRUE);
+  mousemask(REPORT_MOUSE_POSITION, NULL);
+
+  MEVENT e;
+  int px = 2;
+
   mvprintw(12, 30, "Hello World!");
   mvprintw(13, 30, "Press 'q' to exit.");
 
   while (true) {
     int ch = getch();
-    if (ch == 'q')
+    if (ch == 'q') {
       break;
+    }
+
+    if (ch == KEY_MOUSE) {
+      if (getmouse(&e) == OK) {
+        clear();
+        px = e.x;
+        if (px < 2) {
+          px = 2;
+        }
+        if (px > 77) {
+          px = 77;
+        }
+        mvprintw(23, px - 2, "=====");
+        refresh();
+      }
+    }
   }
   endwin();
 }
