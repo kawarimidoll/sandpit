f6ea7e6c9beddfbbf42243b493c1e04bbd3538d6
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-02 15:57:49 +1100

    feat: hide input and cursor

--- c_ncurses/blck.c
+++ c_ncurses/blck.c
@@ -2,7 +2,16 @@
 
 int main(void) {
   initscr();
+
+  // hide key inputs
+  noecho();
+
+  // hide cursor
+  curs_set(0);
+
   mvprintw(12, 30, "Hello World!");
+  mvprintw(13, 30, "Press 'q' to exit.");
+
   while (true) {
     int ch = getch();
     if (ch == 'q')
