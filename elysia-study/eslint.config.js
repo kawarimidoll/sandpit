b1d811b40886e936c71d30d6e1b65759e3c2d4d8
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-15 22:50:39 +1100

    feat: add eslint-plugin-drizzle

--- elysia-study/eslint.config.js
+++ elysia-study/eslint.config.js
@@ -1,8 +1,17 @@
 import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
 
-export default antfu({
-  rules: {
-    'no-console': ['off'],
-    'antfu/no-top-level-await': ['off'],
+const compat = new FlatCompat()
+
+export default antfu(
+  {
+    rules: {
+      'no-console': ['off'],
+      'antfu/no-top-level-await': ['off'],
+    },
   },
-})
+  // Legacy config
+  ...compat.config({
+    extends: ['plugin:drizzle/recommended'],
+  }),
+)
