98624547ca4633d5f5f5768214501fc3b97bb088
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-15 13:34:53 +1100

    feat: add drizzle config

---
+++ elysia-study/drizzle.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit'
+
+export const LOCAL_DB_URL = 'data'
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './src/schema.ts',
+  out: './drizzle',
+  dbCredentials: {
+    url: LOCAL_DB_URL,
+  },
+  driver: 'pglite',
+})
--- elysia-study/src/db.ts
+++ elysia-study/src/db.ts
@@ -1,7 +1,8 @@
 import { PGlite } from '@electric-sql/pglite'
 import { drizzle } from 'drizzle-orm/pglite'
+import { LOCAL_DB_URL } from '../drizzle.config.js'
 
 const client = await PGlite.create({
-  dataDir: 'data',
+  dataDir: LOCAL_DB_URL,
 })
 export const db = drizzle(client)
