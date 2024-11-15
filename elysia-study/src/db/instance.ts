246c9900b1358390c49425463a2b2c90caab9610
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-15 13:36:55 +1100

    chore: rename file

--- elysia-study/src/db/instance.ts
+++ elysia-study/src/db/instance.ts
@@ -1,6 +1,6 @@
 import { PGlite } from '@electric-sql/pglite'
 import { drizzle } from 'drizzle-orm/pglite'
-import { LOCAL_DB_URL } from '../drizzle.config.js'
+import { LOCAL_DB_URL } from '../../drizzle.config.js'
 
 const client = await PGlite.create({
   dataDir: LOCAL_DB_URL,
