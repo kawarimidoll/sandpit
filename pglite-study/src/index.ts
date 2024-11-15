3b575d44db5953cc5952b640d587a2546b8eeb72
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-15 12:37:03 +1100

    feat: update src/index.ts (does not work)

--- pglite-study/src/index.ts
+++ pglite-study/src/index.ts
@@ -1,6 +1,48 @@
+// this doesn't work well :sob:
+
 import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
-import DB_URL from "../../drizzle.config.ts"
+import DB_URL from "../drizzle.config.ts";
+
+import { eq } from "drizzle-orm";
+import { usersTable } from "./db/schema";
 
 const client = new PGlite(DB_URL);
 const db = drizzle({ client });
+
+async function main() {
+  console.log("DB start!");
+
+  const user: typeof usersTable.$inferInsert = {
+    name: "John",
+    age: 30,
+    email: "john@example.com",
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log("New user created!");
+
+  const users = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users);
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log("User info updated!");
+
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log("User deleted!");
+}
+
+main();
