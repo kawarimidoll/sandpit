4b6fbbe0d961021f0a43f51431def220bdea73b7
kawarimidoll <kawarimidoll+git@gmail.com>
2025-01-05 14:58:11 +1100

    feat: add action to server

--- superforms-crud/src/routes/users/[[id]]/+page.server.ts
+++ superforms-crud/src/routes/users/[[id]]/+page.server.ts
@@ -1,7 +1,6 @@
-import { users, userSchema } from '$lib/users';
-
-import { error } from '@sveltejs/kit';
-import { superValidate } from 'sveltekit-superforms';
+import { users, userId, userSchema } from '$lib/users';
+import { error, fail } from '@sveltejs/kit';
+import { message, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 // id is required in userSchema, but it should be null when creating a new user
@@ -19,6 +18,7 @@ export async function load({ params }) {
   const user = users.find(u => u.id === params.id);
 
   if (params.id && !user) {
+    console.log('User not found.');
     throw error(404, 'User not found.');
   }
 
@@ -26,3 +26,34 @@ export async function load({ params }) {
   const form = await superValidate(user, zod(crudSchema));
   return { form, users };
 }
+
+export const actions = {
+  default: async ({ request }) => {
+    const form = await superValidate(request, zod(crudSchema));
+    console.log(form);
+    if (!form.valid) {
+      return fail(400, { form });
+    };
+
+    if (!form.data.id) {
+      // CREATE user
+      // We just push the new user to the array, but in a real app you would save it to a database.
+      const user = { ...form.data, id: userId() };
+      users.push(user);
+
+      return message(form, 'User created!');
+    }
+    else {
+      // UPDATE user
+      // We just update the user in the array, but in a real app you would save it to a database.
+      const index = users.findIndex(u => u.id === form.data.id);
+      if (index < 0) {
+        console.log('User not found.');
+        return error(404, 'User not found.');
+      }
+
+      users[index] = { ...form.data, id: form.data.id };
+      return message(form, 'User updated!');
+    }
+  },
+};
