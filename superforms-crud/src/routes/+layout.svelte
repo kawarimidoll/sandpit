f895b541185a3757a4550fc9e80e3ac8e98cf725
kawarimidoll <kawarimidoll+git@gmail.com>
2025-01-05 14:28:57 +1100

    feat: add global layout

---
+++ superforms-crud/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+  const { children } = $props();
+</script>
+
+<header>
+  <a href='/'>Top</a>
+  <a href='/users'>Users</a>
+</header>
+
+{@render children()}
+
+<style>
+header {
+  border-bottom: 1px solid #ccc;
+  a {
+    display: inline-block;
+    padding: 1rem;
+    &:hover{
+border: none;
+    }
+  }
+}
+</style>
