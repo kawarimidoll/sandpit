af6d862aec6b6c1830867b8cc3a01757224bc1bf
kawarimidoll <kawarimidoll+git@gmail.com>
2025-12-16 15:56:39 +0900

    feat: add basic app structure

--- ratatui_json/src/app.rs
+++ ratatui_json/src/app.rs
@@ -1,1 +1,20 @@
-// app codes
+use std::collections::HashMap;
+
+pub enum CurrentScreen {
+    Main,
+    Editing,
+    Exiting,
+}
+
+pub enum CurrentlyEditing {
+    Key,
+    Value,
+}
+
+pub struct App {
+    pub key_input: String,
+    pub value_input: String,
+    pub pairs: HashMap<String, String>,
+    pub current_screen: CurrentScreen,
+    pub currently_editing: Option<CurrentlyEditing>,
+}
--- ratatui_json/src/main.rs
+++ ratatui_json/src/main.rs
@@ -1,3 +1,8 @@
+// import modules to enable rust-analyzer for now
+mod app;
+use crate::app::App;
+
 fn main() {
     println!("Hello, world!");
+    let _app = App::new();
 }
