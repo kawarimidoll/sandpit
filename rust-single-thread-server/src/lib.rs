61ec8f9ba4709e25447965ef75f168fca4feb698
kawarimidoll <kawarimidoll+git@gmail.com>
2025-12-27 13:20:00 +0900

    feat: send job to worker

--- rust-single-thread-server/src/lib.rs
+++ rust-single-thread-server/src/lib.rs
@@ -8,7 +8,8 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+// executeメソッドが取るクロージャの型のエイリアス
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -31,10 +32,12 @@ impl ThreadPool {
         ThreadPool { workers, sender }
     }
 
-    pub fn execute<F>(&self, _f: F)
+    pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -45,8 +48,13 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
         });
         Worker { id, thread }
     }
